bbb771770e792540521e92ec0aadf61b74d667b9
Sushant <sushantj@markk.app>
2021-06-05 02:30:31 +0530

    Add color picker component and basic argb to int conversion

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Pick a color to begin
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomeComponent msg="Color Converter v0.1"/>
+    <chrome-picker v-model="colors" />
+    <br>
+    <p>
+      <strong>RGBA</strong>
+      {{colors.rgba}}
+    </p>
+    <p>
+      <strong>Int representation</strong>
+      {{getIntFromARGB((colors.rgba.a * 255), colors.rgba.r, colors.rgba.g, colors.rgba.b)}}
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { Chrome } from 'vue-color';
+import HomeComponent from '@/components/HomeComponent.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    HomeComponent,
+    'chrome-picker': Chrome,
+  },
+  data() {
+    return {
+      colors: {
+        hex: '#194d33',
+        hex8: '#194D33A8',
+        hsl: {
+          h: 150, s: 0.5, l: 0.2, a: 1,
+        },
+        hsv: {
+          h: 150, s: 0.66, v: 0.30, a: 1,
+        },
+        rgba: {
+          r: 25, g: 77, b: 51, a: 1,
+        },
+        a: 1,
+      },
+    };
+  },
+  methods: {
+    getIntFromARGB(alpha, red, green, blue) {
+      // eslint-disable-next-line no-bitwise
+      return (alpha << 24) | (red << 16) | (green << 8) | blue;
+    },
   },
 };
 </script>
+
+<style lang="scss">
+  .home {
+    .vc-chrome {
+      width: 100%;
+      max-width: 400px;
+      margin: auto;
+    }
+  }
+</style>
